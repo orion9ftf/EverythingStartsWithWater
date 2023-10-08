935b548f96b012c50703a44540aa4eb0b2b663aa
ewi-orion <brpmdr@mailfence.com>
2023-10-08 00:25:44 -0300

    climate_change_in_the_world and water_cycle files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def water_cycle
+  end
+
+  def climate_change_in_the_world
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'water_cycle', to: "home#water_cycle"
+  get 'climate_change_in_the_world', to: "home#climate_change_in_the_world"
   root "home#index"
 end
